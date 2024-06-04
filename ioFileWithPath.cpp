dd023cbde2e8f6001a8d5661ae3343a84bb86958
wildanindi <wildanindi2@gmail.com>
2024-06-04 17:58:35 +0700

    melanjukan project file with path

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -21,5 +21,36 @@ int main() {
     while (true) {
         cout << "- ";
         //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //lopp akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //Membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt, ios::in");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+
 }
+
