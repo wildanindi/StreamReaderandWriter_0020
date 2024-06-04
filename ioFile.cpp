47b729c8aa5aa48be65c451a9605c09fd35e8a49
wildanindi <wildanindi2@gmail.com>
2024-06-04 17:22:46 +0700

    mengisi method main dengan variabel baris, lalu menggunakan  ofstream dan outfile unutk membuka file dan menunjuk file

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int main () {
-    
+    string baris;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
     
 }
