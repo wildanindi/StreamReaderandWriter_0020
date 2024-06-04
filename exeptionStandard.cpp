3168fd31bd0c645699383220a26fd3e5b127cbfe
wildanindi <wildanindi2@gmail.com>
2024-06-04 18:21:53 +0700

    membuat project baru dengan variabel globalny exception dan array

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -1,1 +1,24 @@
-#
+#include <iostream>
+#include <exception>
+//untuk obyek exeption yang akan digunakan
+#include <array>
+//untuk obyek array yangakan kita gunakan
+using namespace std;
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5 ,7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		//akan dieksekusi karna array data hanya memiliki 3 elemen/
+	}
+	cout << "Baris Program Yang terakhir" << endl;
+	//penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan/
+	return 0;
+}
